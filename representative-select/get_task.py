042f8b0855591efccb4cd87dac1b7bb9fac9ca14
YuliWanghust <44452178+YuliWanghust@users.noreply.github.com>
2024-06-17 22:58:25 -0400

    Update get_task.py

--- representative-select/get_task.py
+++ representative-select/get_task.py
@@ -4,39 +4,6 @@ import random
 from tqdm import tqdm
 from datasets import load_dataset
 
-def format_dataset(sample):
-    question = sample['question']['text']
-    context = sample['document']['tokens']['token']
-    is_html = sample['document']['tokens']['is_html']
-    long_answers = sample['annotations']['long_answer']
-    short_answers = sample['annotations']['short_answers']
-
-    context_string =  " ".join([context[i] for i in range(len(context)) if not is_html[i]])
-
-    # 0 - No ; 1 - Yes
-    for answer in sample['annotations']['yes_no_answer']:
-        if answer == 0 or answer == 1:
-            return {"question": question, "short": ["no" if answer == 0 else "yes"], "long": [], "category": "no" if answer == 0 else "yes"}
-
-    short_targets = []
-    for s in short_answers:
-        short_targets.extend(s['text'])
-    short_targets = list(set(short_targets))
-
-    long_targets = []
-    for s in long_answers:
-        if s['start_token'] == -1:
-            continue
-        answer = context[s['start_token']: s['end_token']]
-        html = is_html[s['start_token']: s['end_token']]
-        new_answer = " ".join([answer[i] for i in range(len(answer)) if not html[i]])
-        if new_answer not in long_targets:
-            long_targets.append(new_answer)
-
-    category = "other" if len(short_targets) > 0 else "null"
-
-    return {"question": question, "short": short_targets, "long": long_targets, "category": category}
-
 def process_mnli_examples(examples):
     processed_examples = []
     idx = 0
